3c549ee8cc9e44e84e113ce8b04451c687a97f66
Anagha856 <anaghamohananp@gmail.com>
2024-09-03 10:45:12 +0530

    Add files via upload

---
+++ anagha/basics.js
@@ -0,0 +1,13 @@
+console.log("temperature")
+temp=45
+if(temp<20)
+{
+    console.log("cold")
+}
+else if(temp<=20 && temp<=30)
+{
+    console.log("normal")
+}
+else{
+    console.log("hot day")
+}
---
+++ anagha/object.js
@@ -0,0 +1,26 @@
+//Nested Objects:
+// Array of three people
+let people = [
+    {
+      name: 'Alice',
+      age: 25,
+      place: {
+        city: 'Wonderland',
+        country: 'Fairyland'
+      }
+    },
+    {
+      name: 'Bob',
+      age: 30,
+      place: {
+        city: 'TechCity',
+        country: 'Geekland'
+      }
+    },
+    {
+      name: 'Charlie',
+      age: 28,
+      place: {object.js
+console.log(people[1].name)
+people[0].age=31
+console.log(people[0])
